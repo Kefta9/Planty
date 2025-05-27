8a1351b1c08524ce1660a554fbfca48eca76440c
Kevin <Vincent.kevin@outlook.com>
2025-05-27 15:50:05 +0200

    Ajout du hook pour le lien Admin

--- wp-content/themes/astra-planty-child/functions.php
+++ wp-content/themes/astra-planty-child/functions.php
@@ -11,6 +11,27 @@ add_action( 'wp_enqueue_scripts', 'astra_planty_child_style' );
 					wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
 				}
 
+add_filter( 'wp_nav_menu_items', function($items, $args) {
+    if ( is_user_logged_in() && current_user_can('administrator') && $args->theme_location == 'primary' ) {
+        $items_array = array();
+
+        while ( false !== ( $item_pos = strpos( $items, '<li', 3 ) ) ) {
+            $items_array[] = substr( $items, 0, $item_pos );
+            $items = substr( $items, $item_pos );
+        }
+        $items_array[] = $items;
+
+        // Insérer le lien Admin en 2e position
+        array_splice( $items_array, 1, 0, '<li class="menu-item menu-admin"><a href="/wp-admin/">Admin</a></li>' );
+
+        $items = implode( '', $items_array );
+    }
+    return $items;
+}, 10, 2 );
+
+
+
+
 /**
  * Your code goes below.
  */
